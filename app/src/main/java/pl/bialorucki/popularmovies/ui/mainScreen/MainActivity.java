975be530fe593defd5f787ca16fd34e94464f072
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-05 13:45:11 +0100

    Configure api key storage

--- app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MainActivity.java
+++ app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MainActivity.java
@@ -3,6 +3,7 @@ package pl.bialorucki.popularmovies.ui.mainScreen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pl.bialorucki.popularmovies.BuildConfig;
 import pl.bialorucki.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity implements MainScreenContract.View{
