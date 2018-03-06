bed97722ee4c0a3af17586f23712faf837653e70
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-06 19:31:52 +0100

    Add recycle view

--- app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MainActivity.java
+++ app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MainActivity.java
@@ -1,16 +1,47 @@
 package pl.bialorucki.popularmovies.ui.mainScreen;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import pl.bialorucki.popularmovies.BuildConfig;
 import pl.bialorucki.popularmovies.R;
+import pl.bialorucki.popularmovies.model.Movie;
+import pl.bialorucki.popularmovies.service.GetMoviesTask;
+import pl.bialorucki.popularmovies.service.MovieRetrofitService;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements MainScreenContract.View{
+
+
+    @BindView(R.id.mainGrid_rv)
+    RecyclerView mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+
+    }
+
+    private void configureRecycleView(){
+        mainGrid.setLayoutManager(new GridLayoutManager(this,2));
+        //mainGrid.setAdapter(new MoviesAdapter());
     }
 }
--- app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MoviesAdapter.java
+++ app/src/main/java/pl/bialorucki/popularmovies/ui/mainScreen/MoviesAdapter.java
@@ -1,8 +1,58 @@
 package pl.bialorucki.popularmovies.ui.mainScreen;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pl.bialorucki.popularmovies.R;
+import pl.bialorucki.popularmovies.model.Movie;
+
 /**
  * Created by Maciej Bialorucki on 06.03.18.
  */
 
-class MoviesAdapter {
+class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    private List<Movie> movies;
+
+    public MoviesAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+
+        @BindView(R.id.title_tv)
+        TextView movieTitle;
+
+        @BindView(R.id.poster_iv)
+        ImageView moviewPoster;
+
+
+    }
 }
