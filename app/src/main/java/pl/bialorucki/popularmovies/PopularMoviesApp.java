f359f27d7b2f0d3f990268ef50de6c44e7d64dec
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-06 19:30:35 +0100

    Add Stetho library

--- app/src/main/java/pl/bialorucki/popularmovies/PopularMoviesApp.java
+++ app/src/main/java/pl/bialorucki/popularmovies/PopularMoviesApp.java
@@ -2,6 +2,8 @@ package pl.bialorucki.popularmovies;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 /**
  * Created by Maciej Bialorucki on 02.03.18.
  */
@@ -11,5 +13,7 @@ public class PopularMoviesApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Stetho.initializeWithDefaults(this);
     }
 }
