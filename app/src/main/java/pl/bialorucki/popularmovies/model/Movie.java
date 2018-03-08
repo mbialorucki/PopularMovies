c82b28efc09fa894a2e96a06390acb945bf0a5b5
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-08 14:12:27 +0100

    Add parcelable implmentation to Movie class

--- app/src/main/java/pl/bialorucki/popularmovies/model/Movie.java
+++ app/src/main/java/pl/bialorucki/popularmovies/model/Movie.java
@@ -1,5 +1,8 @@
 package pl.bialorucki.popularmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 
@@ -7,7 +10,7 @@ import com.google.gson.TypeAdapter;
  * Created by Maciej Bialorucki on 05.03.18.
  */
 
-public  class Movie {
+public  class Movie implements Parcelable {
 
     private String vote_average;
 
@@ -181,4 +184,60 @@ public  class Movie {
     public String toString() {
         return super.toString();
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.vote_average);
+        dest.writeString(this.backdrop_path);
+        dest.writeString(this.adult);
+        dest.writeString(this.id);
+        dest.writeString(this.title);
+        dest.writeString(this.overview);
+        dest.writeString(this.original_language);
+        dest.writeStringArray(this.genre_ids);
+        dest.writeString(this.release_date);
+        dest.writeString(this.original_title);
+        dest.writeString(this.vote_count);
+        dest.writeString(this.poster_path);
+        dest.writeString(this.video);
+        dest.writeString(this.popularity);
+    }
+
+    public Movie() {
+    }
+
+    protected Movie(Parcel in) {
+        this.vote_average = in.readString();
+        this.backdrop_path = in.readString();
+        this.adult = in.readString();
+        this.id = in.readString();
+        this.title = in.readString();
+        this.overview = in.readString();
+        this.original_language = in.readString();
+        this.genre_ids = in.createStringArray();
+        this.release_date = in.readString();
+        this.original_title = in.readString();
+        this.vote_count = in.readString();
+        this.poster_path = in.readString();
+        this.video = in.readString();
+        this.popularity = in.readString();
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
