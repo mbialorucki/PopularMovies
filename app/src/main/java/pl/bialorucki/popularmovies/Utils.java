fb18f59eb41c0b829d5db35f103acb865318653d
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-15 09:31:28 +0100

    Add Utils class

---
+++ app/src/main/java/pl/bialorucki/popularmovies/Utils.java
@@ -0,0 +1,9 @@
+package pl.bialorucki.popularmovies;
+
+/**
+ * Created by Maciej Bialorucki on 08.03.18.
+ */
+
+public class Utils {
+    public static final String BASE_PATH = "http://image.tmdb.org/t/p/w185/"; //base path for movies covers
+}
