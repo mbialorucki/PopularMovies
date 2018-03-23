ade2dbc7895eae76280f612089a353596273d1dc
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2018-03-23 11:35:51 +0100

    Add details presenter tests

---
+++ app/src/test/java/pl/bialorucki/popularmovies/ui/detailScreen/DetailsPresenterTest.java
@@ -0,0 +1,44 @@
+package pl.bialorucki.popularmovies.ui.detailScreen;
+
+import pl.bialorucki.popularmovies.model.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Maciej Bialorucki on 22.03.18.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DetailsPresenterTest {
+
+    @Mock
+    public DetailScreenContract.View mockView;// = mock(DetailScreenContract.View.class);
+
+
+    @Test
+    public void should_show_movie_details_when_movie_is_not_null(){
+        DetailScreenContract.Presenter presenter = new DetailsPresenter();
+        presenter.attachView(mockView);
+
+        Movie movie = new Movie();
+        presenter.loadMovieDetails(movie);
+
+        verify(mockView).showMovieDetails(movie);
+    }
+
+    @Test
+    public void should_show_error_message_when_movie_is_null(){
+        DetailScreenContract.Presenter presenter = new DetailsPresenter();
+        presenter.attachView(mockView);
+
+        presenter.loadMovieDetails(null);
+
+        verify(mockView).showError();
+
+    }
+}
